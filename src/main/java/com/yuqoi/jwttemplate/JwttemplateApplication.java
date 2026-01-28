ad52e7061c1f4e696d831914586d89a1f949bde3
Aleksander <lolwolfyou@gmail.com>
2026-01-29 00:14:28 +0100

    init

---
+++ src/main/java/com/yuqoi/jwttemplate/JwttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.yuqoi.jwttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwttemplateApplication {
+sdasdasd
+	public static void main(String[] args) {
+		SpringApplication.run(JwttemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuqoi/jwttemplate/JwttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuqoi.jwttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwttemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
